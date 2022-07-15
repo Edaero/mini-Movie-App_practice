6affb8a7f001c0f0744ee1a5df3756ee5dafe75e
Deng Lee <edaero22@naver.com>
2022-07-15 18:07:17 +0900

    modify redirect & rewrite

--- next.config.js
+++ next.config.js
@@ -4,34 +4,27 @@ const API_KEY = "eaeaea96310ad18633d659d09d755f99"
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  // API KEY를 숨기지 않는 redirect
-  // redirection에 필요한 코드
-  async redirects() {
+  async redirects() {  // redirect 문법, API KEY를 숨기지 않는다.
     return [
       {
-        // 주소 뒤에 별을 붙이면 뒤에 붙은 모든 걸 그대로 가져올 수 있다.
-        // source를 찾아서 유저가 URL로 어디론가 이동한다면
-        source: "/old-blog/:path*",
-        // form이라는 destination에 보낸다
-        destination: "/new-sexy-blog/:path*",
-        // 여기서 이 redirection이 영구적인지 아닌지에 따라 검색엔진이 이 정보를 기억하는지 여부가 결정된다.
-        permanent: false,
-      }, // redirect를 추가하고 싶다면 뒤에 또 써주면 된다.
-      {
-        source: "/contact",
-        destination: "/no-contact",
+        // path는 임의의 id값이나 string 등 어떤 값이든 들어갈 수 있다.
+        // path에 *을 붙이면 뒤에 붙는 모든 값이든 가져올 수 있다.
+        source: "/old-blog/:path*", // source는 유저가 URL로 접근하는 주소
+        destination: "/new-blog/:path*", // source로 접근하면 destination에 있는 주소로 변경해 보낸다.
+        permanent: false, // redirection이 영구적인지 아닌지에 따라 검색엔진이 이 정보를 기억 여부를 결정
+      },
+      { // redirect를 추가하고 싶다면 뒤에 또 써주면 된다.
+        source: "/bofore-contact/:path*",
+        destination: "/after-contact/:path*",
         permanent: false,
       },
     ];
   },
-  // API KEY를 숨기는 rewrites
-  // rewrites는 redirect시키기는 하지만 URL이 변하지 않는다.
-  async rewrites() {
+  // rewrites를 사용하면 API KEY를 숨길 수 있다.
+  async rewrites() { // rewrites는 redirect시키기는 하지만 URL이 변하지 않는다.
     return [
       {
-        // source의 주소를 fetch 주소에 옮겨준다.
-        source: "/api/movies",
+        source: "/api/movies", // source의 주소를 fetch 주소에 옮겨준다.
         destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
       }
     ]
