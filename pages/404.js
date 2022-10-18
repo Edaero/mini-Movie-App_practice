3951b20802b5c7e5be05939cc5c2eb64bdd12a3b
Deng <edaero22@naver.com>
2022-10-18 14:48:33 +0900

    404 modi

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,42 @@
+import Link from "next/link";
+
 export default function NotFound() {
-    return "잘못된 경로입니다."
+  return (
+    <>
+      <div>
+        <h1>!!! Invalid Path !!!</h1>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </div>
+      <style jsx>{`
+        div {
+          height: 800px;
+          text-align: center;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          gap: 10px;
+        }
+        h1 {
+          color: red;
+          background-color: darkgray;
+          padding: 5px;
+          border-radius: 5px;
+        }
+        a {
+          font-size: 40px;
+          padding: 5px;
+          border-radius: 5px;
+          color: black;
+          text-decoration: none;
+        }
+        a:hover {
+          color: white;
+          background-color: darkgray;
+        }
+      `}</style>
+    </>
+  );
 }
